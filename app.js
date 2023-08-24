8c489f668cef089f4a72d7d4b15ce72c2c68c137
shedrackejike <shedrackejike3@gmail.com>
2023-08-24 23:27:02 +0100

    updated

--- app.js
+++ app.js
@@ -4,43 +4,6 @@ let cards = document.getElementsByClassName("cards")[0];
 let search = document.getElementsByClassName("search")[0];
 let search_input = document.getElementById("search_input");
 
-let signupBtn  = document.querySelector("#signup");
-
-let formOpenBtn = document.querySelector("#form-open"),
-home = document.querySelector(".home"),
-formContainer = document.querySelector(".form_container"),
-formCloseBtn = document.querySelector(".form_close"),
-loginBtn  = document.querySelector("#signup"),
-pwShowHide  = document.querySelector(".pw_hide");
-
-formOpenBtn.addEventListener("click", () => home.classList.add("show"));
-formCloseBtn.addEventListener("click", ()=> home.classList.remove("show"))
-
-pwShowHide.forEach(icon =>{
-  icon.addEventListener("click", () =>{
-    let getPwInput = icon.parentElement.querySelector("input")
-    if (getPwInput.type === "password") {
-      getPwInput.type = "text";
-      icon.classList.replace("bi-file-lock2-fill", "bi-eye-slash")
-    } else {
-      getPwInput.type = "password";
-      icon.classList.replace("bi-eye-slash", "bi-eye-slash")
-    }
-  })
-})
-
-
-
-signupBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  console.log(signupBtn)
-
-  formContainer.classList.add("active");
-});
-loginBtn.addEventListener("click", (e) =>{
-  e.preventDefault();
-  formContainer.classList.remove("active")
-})
 
 
 
---
+++ main.js
@@ -0,0 +1,73 @@
+let closeButton = document.querySelector(".form_close");
+let regSignup = document.querySelector(".signup_form");
+let seePassword = document.querySelectorAll(".pw_hide");
+let restoreLogin = document.querySelector(".buttonLogin");
+let loginform = document.querySelector("#form_container");
+let logform = document.getElementById("signin");
+let regform = document.getElementById("signup");
+let Registerationform = document.getElementById("regFormInput");
+let LoginformDamn = document.getElementById("loginFormInput");
+// console.log(regform);
+
+closeButton.onclick = function () {
+  loginform.hidden = true;
+};
+
+restoreLogin.onclick = function () {
+  loginform.hidden = false;
+};
+
+regform.onclick = function () {
+  LoginformDamn.hidden = true;
+  Registerationform.hidden = false;
+};
+
+logform.onclick = function () {
+  LoginformDamn.hidden = false;
+  Registerationform.hidden = true;
+};
+
+seePassword.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    let getPwInput = icon.parentElement.querySelector("input");
+    if (getPwInput.type === "password") {
+      getPwInput.type = "text";
+      icon.classList.replace("bi-file-lock2-fill", "bi-eye-slash");
+    } else {
+      getPwInput.type = "password";
+      icon.classList.replace("bi-eye-slash", "bi-eye-slash");
+    }
+  });
+});
+
+
+
+//   const form1 = document.querySelector(".form1");
+const regFunc = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    localStorage.setItem(`${email}`,JSON.stringify({email,password}));
+    alert("Registered Successfully");
+
+   
+}
+
+const logfunc = (e) =>{
+    //  For login
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    let user = localStorage.getItem(`${email}`);
+    if (user) {
+        user = JSON.parse(user);
+        if (user.password == password) {
+            localStorage.setItem("auth", "true");
+            window.location.href = "http://127.0.0.1:5501/movie.html" //the url of the homepage
+        } else {
+            alert("Invalid Credentials")
+        }
+    } else {
+        alert("Sign Up")
+    }
+}
